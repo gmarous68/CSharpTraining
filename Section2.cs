1de4bb8668103c3430d289f55e2c02ce01458c37
Gerald Marous <gmarous@hotmail.com>
2025-04-23 11:59:53 +0200

    Section2 added

---
+++ Section2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    class Section2
+    {
+        public static void App()
+        {
+            int num;
+
+            Console.WriteLine("Type in a number: " );
+            string numString = Console.ReadLine();
+            bool isNum = int.TryParse(numString, out num);
+            if(isNum)
+                Console.WriteLine("OK - it's a number");
+            else
+                Console.WriteLine("NOT OK - it's not a number");
+            Console.WriteLine("*********************************");
+
+            Console.ReadKey();
+        }
+        
+    }
+}

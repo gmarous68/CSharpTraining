3f2d053fb9f75103b9074d316064345770a9a225
Gerald Marous <66553716+gmarous68@users.noreply.github.com>
2025-04-23 18:43:51 +0200

    Create Section3.cs
    New file

---
+++ Section3.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    class Section3
+    {
+    }
+}

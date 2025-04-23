33bdd569211cead80ca0bc06508505f2912d3732
Gerald Marous <gmarous@hotmail.com>
2025-04-23 17:21:19 +0200

    Section2 OK

--- Section2.cs
+++ Section2.cs
@@ -13,15 +13,27 @@ namespace CSharpTraining
         {
             int num;
 
-            Console.WriteLine("Type in a number: " );
+            Console.WriteLine("Type in a number: ");
             string numString = Console.ReadLine();
             bool isNum = int.TryParse(numString, out num);
-            if(isNum)
+            if (isNum)
                 Console.WriteLine("OK - it's a number");
             else
                 Console.WriteLine("NOT OK - it's not a number");
             Console.WriteLine("*********************************");
 
+            Random ran = new Random();
+            while (true)
+            {
+                int guessNum;
+                int ranNum = ran.Next(1, 11);
+                Console.WriteLine("Guess a number..:");
+                bool isCorrect = int.TryParse(Console.ReadLine(), out guessNum);
+                if (isCorrect && (guessNum == ranNum)) {
+                    Console.WriteLine("You guessed right!");
+                    break;
+                }                
+            }
             Console.ReadKey();
         }
         

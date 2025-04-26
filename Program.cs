8d925a731068b85482367dbd073e92d4b7d11325
Gerald Marous <gmarous@hotmail.com>
2025-04-26 17:49:56 +0200

    Section5 added

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@
         static void Main(string[] args)
         {
             // Start of program
-            Section3.App();                       
+            Section5 myCar1 = new Section5("BMW", "i7");
+            Section5 myCar2 = new Section5("Audi", "A3");
+
+            Console.WriteLine($"Car 1 is a {myCar1.Brand}/{myCar1.Model}.");
+            Console.WriteLine($"Car 2 is a {myCar2.Brand}/{myCar2.Model}.");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Section5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    class Section5
+    {
+        public Section5(string brand, string model)
+        {
+            this.Brand = brand;
+            this.Model = model;
+        }
+
+        public string Model { get; set; }
+        public string Brand { get; set; }
+
+    }
+}

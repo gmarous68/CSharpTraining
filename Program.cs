a846e97ee06a57952fc54ba451db5020719e921e
Gerald Marous <gmarous@hotmail.com>
2025-04-28 18:26:42 +0200

    Added to Section6

--- Program.cs
+++ Program.cs
@@ -5,15 +5,12 @@
         static void Main(string[] args)
         {
             // Start of program
-            Section5 myCar1 = new Section5("BMW", "i7");
-            Section5 myCar2 = new Section5("Audi", "A3");
-            Section5 myCar3 = new Section5("Porsche", "Carrera");
-
-            Console.WriteLine($"Car 1 is a {myCar1.Brand}/{myCar1.Model} with id: {myCar1.Id}");
-            Console.WriteLine($"Car 2 is a {myCar2.Brand}/{myCar2.Model} with id: {myCar2.Id}");
-            Console.WriteLine($"Car 3 is a {myCar3.Brand}/{myCar3.Model} with id: {myCar3.Id}");
+            Section6.MyList();
+            Section6.MyLambda();
 
             Console.ReadKey();
         }
+
+        
     }
 }
---
+++ Section6.cs
@@ -0,0 +1,55 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    class Section6
+    {
+        public static void MyList()
+        {
+            List<int> numbers = new List<int>() { 10, 5, 15, 3, 9,25,18 };
+
+            numbers.Sort();
+
+            foreach (int number in numbers)
+                Console.Write(number + " ");
+            Console.WriteLine();
+
+            Console.WriteLine("***********************");
+        }
+
+        public static void MyLambda()
+        {
+            List<int> numbers = new List<int>() { 10, 5, 15, 3, 9, 25, 18 };
+            Predicate<int> equalTenOrHigher = x => x >= 10;
+
+            foreach(int number in numbers.FindAll(equalTenOrHigher))
+                Console.Write(number + " ");
+            Console.WriteLine();
+            Console.WriteLine("***********************");
+                        
+            Predicate<int> lessThanTen = LessThanTen;
+
+            foreach (int number in numbers.FindAll(lessThanTen))
+                Console.Write(number + " ");
+            Console.WriteLine();
+            Console.WriteLine("***********************");
+
+            bool hasLargeNumber = numbers.Any(x => x > 20);
+            if(hasLargeNumber)
+                Console.WriteLine("There are large numbers in the list!");
+            else
+                Console.WriteLine("There are not large numbers in the list!");
+        }
+
+        public static bool LessThanTen(int x)
+        {
+            return x < 10;
+        }
+    }
+}

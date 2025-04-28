eed31a0941348b0b8337829d646cd8da35fee7e1
Gerald Marous <gmarous@hotmail.com>
2025-04-28 14:17:18 +0200

    Section 5 done

--- Section5.cs
+++ Section5.cs
@@ -8,14 +8,19 @@ namespace CSharpTraining
 {
     class Section5
     {
+        private readonly int _id = 0;
+        private static int nextId = 1;
+
         public Section5(string brand, string model)
         {
             this.Brand = brand;
             this.Model = model;
+            this._id = nextId++;
         }
 
         public string Model { get; set; }
         public string Brand { get; set; }
 
+        public int Id => _id;
     }
 }

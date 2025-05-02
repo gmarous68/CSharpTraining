74e8b66d3d419df3c251040884dcf5b9289a40d5
gmarous <gmarous@hotmail.com>
2025-05-02 12:28:23 +0200

    Creation/Initialization of Section17

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Section8.App();
+            Section17.App();
 
             Console.ReadKey();
         }        
---
+++ Section17.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace CSharpTraining
+{
+    internal class Section17
+    {
+        public static void App()
+        {
+            int[] numbers = new int[] { 1, 15, 16, 77, 32, 33, 99, 54, 23, 81, 19 };
+            OddNumber(numbers);
+        }
+
+        public static void OddNumber(int[] numbers)
+        {
+            Console.WriteLine("Odd numbers..");
+
+            IEnumerable<int> oddNumbers = from number in numbers                                          
+                                          where number % 2 != 0
+                                          orderby number descending
+                                          select number;
+            foreach (int number in oddNumbers)
+                Console.Write(number + " ");
+            Console.WriteLine();
+        }
+    }
+}
